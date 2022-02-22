dd9327e997ca37fa994082039e5929ea877273b8
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2022-02-22 14:29:01 +0100

    Basic settings MVVM

---
+++ SwiftUIMarvelApi/View/CharactersView.swift
@@ -0,0 +1,22 @@
+//
+//  CharactersView.swift
+//  SwiftUIMarvelApi
+//
+//  Created by Джон Костанов on 22/2/22.
+//
+
+import SwiftUI
+
+struct CharactersView: View {
+    @EnvironmentObject var homeData: HomeViewModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharactersView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
