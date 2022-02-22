4a460c85b3ffb3d797c728a7b3b122274ff67ac9
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2022-02-22 13:25:09 +0100

    Initial Commit

---
+++ SwiftUIMarvelApi/SwiftUIMarvelApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMarvelApiApp.swift
+//  SwiftUIMarvelApi
+//
+//  Created by Джон Костанов on 22/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMarvelApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
