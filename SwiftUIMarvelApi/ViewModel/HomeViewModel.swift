07bd41eb1efffec74dad368658f37c4b8097350f
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2022-02-23 14:48:34 +0100

    Completed search bar

--- SwiftUIMarvelApi/ViewModel/HomeViewModel.swift
+++ SwiftUIMarvelApi/ViewModel/HomeViewModel.swift
@@ -6,8 +6,31 @@
 //
 
 import SwiftUI
+import Combine
 
 class HomeViewModel: ObservableObject {
     
     @Published var searchQuery = ""
+    
+    // Combine Framework Search Bar...
+    // used to cancel the search publisher when ever we need...
+    var searchCancellable: AnyCancellable? = nil
+    
+    init() {
+        // since SwiftUI uses @published so its a publisher...
+        // so we dont need to explicity define publisher...
+        searchCancellable = $searchQuery
+        // removing duplicate typings...
+            .removeDuplicates()
+            // we dont need to fetch for every typing.
+            .debounce(for: 0.6, scheduler: RunLoop.main)
+            .sink(receiveValue: { str in
+                if str == "" {
+                    // reset Data...
+                } else {
+                    // search Data...
+                    print(str)
+                }
+            })
+    }
 }
