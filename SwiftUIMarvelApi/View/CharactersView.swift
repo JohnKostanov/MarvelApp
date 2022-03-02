09ed118f9de1215b7217fbb72b0032203ea4cf0c
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2022-03-02 18:06:40 +0100

    Added links

--- SwiftUIMarvelApi/View/CharactersView.swift
+++ SwiftUIMarvelApi/View/CharactersView.swift
@@ -64,11 +64,12 @@ struct CharacterRowView: View {
     var character: Character
     
     var body: some View {
-        HStack(alignment: .top, spacing: 30) {
+        HStack(alignment: .top, spacing: 15) {
             WebImage(url: URL(string: character.smallImagePath))
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .frame(width: 150, height: 150)
+                            .cornerRadius(8)
             
             VStack(alignment: .leading, spacing: 8) {
                 Text(character.name)
@@ -79,8 +80,32 @@ struct CharacterRowView: View {
                     .foregroundColor(.gray)
                     .lineLimit(4)
                     .multilineTextAlignment(.leading)
+                
+                // Links
+                HStack(spacing: 10) {
+                    ForEach(character.urls, id: \.self) { data in
+                        NavigationLink(destination: WebView(url: extractURL(data: data))
+                            .navigationTitle(extractURLType(data: data)),
+                        label: {
+                            Text(extractURLType(data: data))
+                        })
+                    }
+                }
             }
             Spacer(minLength: 0)
         }
+        .padding(.horizontal)
+    }
+    
+    func extractURL(data: [String: String]) -> URL {
+        let url = data["url"] ?? ""
+        
+        return URL(string: url)!
+    }
+    
+    func extractURLType(data: [String: String]) -> String {
+        let type = data["type"] ?? ""
+        
+        return type.capitalized
     }
 }
--- SwiftUIMarvelApi/View/WebView.swift
+++ SwiftUIMarvelApi/View/WebView.swift
@@ -5,4 +5,21 @@
 //  Created by Джон Костанов on 2/3/22.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    var url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let view = WKWebView()
+        view.load(URLRequest(url: url))
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+    }
+}
